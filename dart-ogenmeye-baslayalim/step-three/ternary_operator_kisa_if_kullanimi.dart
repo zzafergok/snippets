4451a5f4553ff09339957136f842ec722d2b992f
zzafergok <zfrgk96@gmail.com>
2020-08-27 22:18:20 +0300

    step-three
    ternary operatör kısa if kullanımı

---
+++ dart-ogenmeye-baslayalim/step-three/ternary_operator_kisa_if_kullanimi.dart
@@ -0,0 +1,29 @@
+main(List<String> args) {
+  int sayi1 = 10;
+  int sayi2 = 12;
+  var kucukSayi;
+
+  if (sayi1 < sayi2) {
+    kucukSayi = sayi1;
+  } else {
+    kucukSayi = sayi2;
+  }
+
+  print("kücük olan sayi $kucukSayi dur");
+
+  sayi1 < sayi2 ? kucukSayi = sayi1 : kucukSayi = sayi2;
+  print("kücük : $kucukSayi");
+
+  kucukSayi = (sayi1 < sayi2) ? sayi1 : sayi2;
+  print("kucukSayi : $kucukSayi");
+
+  String isim = "zafer";
+  String soyisim;
+  String mesaj;
+
+  // çift soru işareti koyulmasının sebebi; null değer dönmesin yerine boş değer
+  //dönsün diye yapılan bir ifade
+  mesaj = isim ?? soyisim;
+
+  print("merhaba $mesaj");
+}
