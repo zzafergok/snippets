d78922f92a750abc0b83e4742afd15a2b32b3636
zzafergok <zfrgk96@gmail.com>
2020-09-01 12:33:01 +0300

    list generate and map
    28-step

---
+++ dart-ogenmeye-baslayalim/28-step/list_generate_and_map.dart
@@ -0,0 +1,50 @@
+import 'dart:math';
+
+import '../18-step/ogrenci.dart';
+
+main(List<String> args) {
+  /* List<int> ogrenciNumaralari =
+      List.generate(30, (index) => rastgeleOgrenciNoOlustur()); */
+
+  List<int> ogrenciNumaralari = List.generate(30, (index) {
+    return rastgeleOgrenciNoOlustur();
+  });
+  /* ogrenciNumaralari
+        .forEach((oankiNumara) => print("O anki Numara : $oankiNumara")); */
+
+  //uzun yazılımı
+  /* List<Ogrenci> tumogrenciler = ogrenciNumaralari.map((ogrNo) {
+    return Ogrenci("öğrenci $ogrNo Adı", ogrNo);
+  }).toList(); */
+
+  //kısa yazılımı
+  List<Ogrenci> tumogrenciler = ogrenciNumaralari
+      .map((ogrNo) => Ogrenci("Öğrenci $ogrNo Adı", ogrNo))
+      .toList();
+
+  tumogrenciler.forEach(
+      (oankiOgrenci) => print("O anki Öğrenci No : ${oankiOgrenci.no}"));
+}
+
+int rastgeleOgrenciNoOlustur() {
+  int olusturulanSayi = Random().nextInt(60);
+
+  if (olusturulanSayi != 0) {
+    return olusturulanSayi;
+  } else {
+    var yeniSayi = rastgeleOgrenciNoOlustur();
+    return yeniSayi;
+  }
+}
+
+class Ogrenci {
+  String ad;
+  int no;
+
+  Ogrenci(this.ad, this.no);
+
+  @override
+  String toString() {
+    return "$ad ve numarası $no";
+  }
+}
