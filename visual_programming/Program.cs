f4e3a8c2461b3f725c543415f0e724a420b56848
zzafergok <41114328+zzafergok@users.noreply.github.com>
2020-11-17 13:15:10 +0300

    learning visual programming

---
+++ visual_programming/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace visual_programming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
